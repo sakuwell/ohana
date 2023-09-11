4aa8614b77d29fa00463ce845cff013203578d1d
yukako1110 <amaoumama.150426161219@gmail.com>
2023-09-11 15:59:06 +0900

    変更途中

--- PepeOhana/src/main/java/model/SelectUserBL.java
+++ PepeOhana/src/main/java/model/SelectUserBL.java
@@ -1,5 +1,13 @@
 package model;
 
 public class SelectUserBL {
+	
+	public UsersInfoDto excuteSelectSurvey(int id)  {
+		
+		UsersInfoDao dao = new UsersInfoDao();
+		UsersInfoDto dto = dao.doSelectOne(id);
+		
+		return dto;
+	}
 
 }
