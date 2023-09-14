b19fe276122f5409fdddc904491054bda0618831
Reiko0232 <reiko_a_t@icloud.com>
2023-09-14 16:18:29 +0900

    Date関連変更

--- PepeOhana/src/main/java/model/CatsInfoDto.java
+++ PepeOhana/src/main/java/model/CatsInfoDto.java
@@ -17,8 +17,10 @@ public class CatsInfoDto {
 			private Float		weight;		//ネコの体重
 			private byte[]	image;	//ネコの写真
 			private String 	comment;	//飼い主のコメント
-			private Timestamp 	update;	//投稿日
+			private Timestamp 	up_Date;	//更新日
+			private Timestamp 	reg_Date;	//投稿日
 			private String	userName;	//飼い主の名前
+			private int		Id;	//飼い主の登録番号
 
 
 			//----------------------------------------------------------------
@@ -66,13 +68,21 @@ public class CatsInfoDto {
 			public String getComment() { return comment; }
 			public void setComment(String comment) { this.comment = comment; }
 			
-			public Timestamp getUpDate() { return update; }
-			public void setUpDate(Timestamp update) { this.update = update; }
+			//getter/setter（対象フィールド：Up_Dte：更新日）			
+			public Timestamp getUp_Date() { return up_Date; }
+			public void setUp_Date(Timestamp up_date) { this.up_Date = up_date; }
+			
+			//getter/setter（対象フィールド：Reg_Dte：登録日）			
+			public Timestamp getReg_Date() { return reg_Date; }
+			public void setReg_Date(Timestamp reg_date) { this.reg_Date = reg_date; }
 			
 			//getter/setter（対象フィールド：userName）
 			public String getUserName() { return userName; }
 			public void setUserName(String userName) { this.userName = userName; }
 			
+			//getter/setter（対象フィールド：userId）
+			public int getId() { return Id; }
+			public void setId(int Id) { this.Id = Id; }
 		
 
 }
