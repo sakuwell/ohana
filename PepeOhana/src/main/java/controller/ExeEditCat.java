ca5502c9168b0955df28d7c41bcf94d88b810872
Reiko0232 <reiko_a_t@icloud.com>
2023-09-19 16:35:43 +0900

    0919Bye

--- PepeOhana/src/main/java/controller/ExeEditCat.java
+++ PepeOhana/src/main/java/controller/ExeEditCat.java
@@ -1,16 +1,24 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.CatsInfoDto;
+import model.EditCatOneBL;
+import model.UsersInfoDto;
 
 /**
  * Servlet implementation class ExeEditCat
  */
 @WebServlet("/ExeEditCat")
+@MultipartConfig
 public class ExeEditCat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -28,6 +36,44 @@ public class ExeEditCat extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+
+		response.setContentType("text/html;charset=UTF-8");
+	
+//	リクエスト（受信データ）の文字コードを設定
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session           = request.getSession();
+		UsersInfoDto userInfoOnSession = (UsersInfoDto)session.getAttribute("LOGIN_INFO");
+		
+
+		int userId = userInfoOnSession.getID(); 
+		
+		System.out.println(userId);
+		
+		EditCatOneBL logic = new EditCatOneBL();
+		CatsInfoDto editCatOne = logic.exeSelectOneCatInfo(userId);
+		
+		
+		
+		request.setAttribute("cat", editCatOne);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
 	}
 
 	/**
