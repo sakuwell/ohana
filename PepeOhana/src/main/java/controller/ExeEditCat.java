8783d1ae90b6dd998818dbef949d926baa739a49
Reiko0232 <reiko_a_t@icloud.com>
2023-09-20 16:03:31 +0900

    getwriter削除・・文字化け解消

--- PepeOhana/src/main/java/controller/ExeEditCat.java
+++ PepeOhana/src/main/java/controller/ExeEditCat.java
@@ -38,7 +38,7 @@ public class ExeEditCat extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 	
 		System.out.println("OK");
 
@@ -94,7 +94,12 @@ public class ExeEditCat extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+//		リクエスト（受信データ）の文字コードを設定
+		request.setCharacterEncoding("UTF-8");
 	}
 
 }
