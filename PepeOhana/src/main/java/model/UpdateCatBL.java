fbe98bf0062e85e0e1b9e1425e34030c147680db
Reiko0232 <reiko_a_t@icloud.com>
2023-09-25 13:35:33 +0900

    追加分

---
+++ PepeOhana/src/main/java/model/UpdateCatBL.java
@@ -0,0 +1,13 @@
+package model;
+
+public class UpdateCatBL {
+	
+	public boolean exeUpdateCat(CatsInfoDto dto) {
+		
+		boolean   succesUpdate = false ; 
+		
+		CatsInfoDao dao = new CatsInfoDao();
+		succesUpdate = dao.doUpdateCat(dto);
+		
+		return succesUpdate;
+}
