0a1f0732bbe561849cbf978d3f120877138c92b1
katooosan <b.o.c.cha0956@gmail.com>
2023-09-25 15:53:04 +0900

    ExeDelMessage更新

--- PepeOhana/src/main/java/controller/ExeDelMessage.java
+++ PepeOhana/src/main/java/controller/ExeDelMessage.java
@@ -31,6 +31,9 @@ public class ExeDelMessage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		
 		String messageId = request.getParameter("MESSAGEID");
 		
 		MessagesDto dto = new MessagesDto();
