9aa7d4e7a7695bb65d1a480570337d31e7e98154
Reiko0232 <reiko_a_t@icloud.com>
2023-09-28 14:54:09 +0900

    DEL実施のために作成

---
+++ PepeOhana/src/main/java/controller/ExeDelCat.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.UpdateDelCatBL;
+
+/**
+ * Servlet implementation class ExeDelCat
+ */
+@WebServlet("/ExeDelCat")
+public class ExeDelCat extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExeDelCat() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+//		リクエスト（受信データ）の文字コードを設定
+		request.setCharacterEncoding("UTF-8");
+		
+		System.out.println("doPostUTF-8の下");
+		
+//		パラメータ取得
+		
+		String catIdStr = request.getParameter("CATID");		
+		System.out.println(catIdStr);		
+		int catId = Integer.parseInt(catIdStr);
+		
+		//削除のためDELに１を登録
+		UpdateDelCatBL logic = new UpdateDelCatBL();
+		boolean succesUpdate = logic.exeUpdateDelCat(catId); 
+
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
